ab83a3d5103116bb13462ef19e79534c5d34d3a5
sqweeq <54276533+sqweeq@users.noreply.github.com>
2022-06-28 01:43:16 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -6,24 +6,84 @@ import part2 from "../part2.json";
 export default function App() {
   const [sampleStock, setSampleStock] = useState(null);
   const [allStocks, setAllStocks] = useState([]);
+  const [filterSymbol, setFilterSymbol] = useState("");
+  const [filterMax, setFilterMax] = useState("");
+  const [filterMin, setFilterMin] = useState("");
+  const [allFilteredStocks, setAllFilteredStocks] = useState([]);
+  useEffect(() => {
+    setFiltered();
+  }, [filterSymbol, filterMin, filterMax]);
 
   useEffect(() => {
     setSampleStock(part1);
     setAllStocks(part2);
-    console.log(allStocks);
   }, []);
+  function setFiltered() {
+    console.log(filterSymbol);
+    // console.log(filterMin);
+    // console.log(filterMax);
+
+    const newFiltered = allStocks.filter((item) =>
+      filterSymbol !== ""
+        ? item.symbol === filterSymbol
+        : item.regularMarketPrice.raw > Number(filterMin) &&
+          item.regularMarketPrice.raw < Number(filterMax)
+    );
+
+    setAllFilteredStocks(newFiltered);
+    console.log(allFilteredStocks);
+  }
   if (sampleStock && allStocks)
     return (
       <div className="App">
         <h1>Hello CodeSandbox</h1>
         {/* <StockRow sample={sampleStock} /> */}
+
+        <h2>filter stock</h2>
+        <label>
+          Enter your name:
+          <input
+            type="text"
+            value={filterSymbol}
+            onChange={(e) => {
+              setFilterSymbol(e.target.value);
+              setFiltered();
+            }}
+          />
+        </label>
+        <label>
+          Enter your min:
+          <input
+            type="text"
+            value={filterMin}
+            onChange={(e) => {
+              setFilterMin(e.target.value);
+              setFiltered();
+            }}
+          />
+        </label>
+        <label>
+          Enter your max:
+          <input
+            type="text"
+            value={filterMax}
+            onChange={(e) => {
+              setFilterMax(e.target.value);
+              setFiltered();
+            }}
+          />
+        </label>
+        <FilterableStockTable stocks={allFilteredStocks} />
+
+        <h2>All stock</h2>
+
         <StockTable stocks={allStocks} />
       </div>
     );
 }
 
 function StockRow(props) {
-  console.log(props.sample.regularMarketPrice.raw);
+  // console.log(props.sample.regularMarketPrice.raw);
   return (
     <tr>
       <td>{props.sample.symbol}</td>
@@ -34,7 +94,7 @@ function StockRow(props) {
 }
 
 function StockTable(props) {
-  console.log(props.stocks[0]);
+  // console.log(props.stocks[0]);
   return (
     <table>
       <tr>
@@ -43,7 +103,22 @@ function StockTable(props) {
         <th>Value</th>
       </tr>
       {props.stocks.map((item, idx) => {
-        return <StockRow sample={item} />;
+        return <StockRow key={idx} sample={item} />;
+      })}
+    </table>
+  );
+}
+
+function FilterableStockTable(props) {
+  return (
+    <table>
+      <tr>
+        <th>Symbol</th>
+        <th>Security</th>
+        <th>Value</th>
+      </tr>
+      {props.stocks.map((item, idx) => {
+        return <StockRow key={idx} sample={item} />;
       })}
     </table>
   );
